da418351e9588d0ecee2e28d02f335e15afd1993
DaGameGamer <dagamegamer6@gmail.com>
2020-06-13 08:16:32 +1200

    2.0.2

--- disablevolume/LLLRootListController.m
+++ disablevolume/LLLRootListController.m
@@ -19,7 +19,7 @@
 
 -(void)sourceCode{
 	[[UIApplication sharedApplication]
-	openURL:[NSURL URLWithString:@"https://github.com/conf1rmed/disablevolume"]
+	openURL:[NSURL URLWithString:@"https://github.com/conf1rmed"]
 	options:@{}
 	completionHandler:nil];
 }
